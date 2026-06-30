83e0104e3f0cd5597f0c39a047bbb8185b3332af
Chirag Gajjar <gajjarc@google.com>
2026-06-30 10:22:05 +0000

    Use print for URL debugging

--- src/google/appengine/api/taskqueue/taskqueue.py
+++ src/google/appengine/api/taskqueue/taskqueue.py
@@ -2253,8 +2253,7 @@ class Queue(object):
       relative_uri = '/' + relative_uri
       
     url = f"https://{url_host}{relative_uri}"
-    import logging
-    logging.info(f"Jetski: Constructed Cloud Tasks URL: {url}")
+    print(f"Jetski: Constructed Cloud Tasks URL: {url}", flush=True)
 
     http_method = tasks_v2beta3.HttpMethod.POST
     if task.method:
