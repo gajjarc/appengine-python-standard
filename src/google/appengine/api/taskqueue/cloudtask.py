e9db6fa06939eb354f269c1d3deba9363959618b
Chirag Gajjar <gajjarc@google.com>
2026-07-02 05:45:53 +0000

    Refactor Cloud Tasks logic into cloudtask.py

---
+++ src/google/appengine/api/taskqueue/cloudtask.py
@@ -0,0 +1,184 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Cloud Tasks backend integration for Taskqueue SDK."""
+
+import datetime
+import os
+import urllib.request
+from google.api_core import exceptions as google_exceptions
+from google.appengine.api import app_identity
+from google.cloud import tasks_v2beta3
+from google.protobuf.timestamp_pb2 import Timestamp
+
+
+class _DummyRPC(object):
+  """A dummy RPC object to wrap synchronous calls for async compatibility."""
+
+  def __init__(self, result_provider):
+    self._result_provider = result_provider
+
+  def get_result(self):
+    return self._result_provider()
+
+  def wait(self):
+    pass
+
+  def check_success(self):
+    pass
+
+
+def _get_region():
+  """Determines the App Engine region."""
+  region = os.environ.get('GAE_REGION')
+  if region:
+    return region
+
+  try:
+    req = urllib.request.Request(
+        'http://metadata.google.internal/computeMetadata/v1/instance/zone',
+        headers={'Metadata-Flavor': 'Google'}
+    )
+    with urllib.request.urlopen(req, timeout=1) as response:
+      zone = response.read().decode('utf-8')
+      if '/' in zone:
+        zone = zone.split('/')[-1]
+      region = zone.rsplit('-', 1)[0]
+      return region
+  except Exception:
+    pass
+
+  # Fallback to us-central1 if we can't detect it
+  return 'us-central1'
+
+
+def create_task_in_cloud_tasks(queue_name, task, multiple):
+  """Calls Cloud Tasks API to create a task."""
+  client = tasks_v2beta3.CloudTasksClient()
+  project = os.environ.get('GOOGLE_CLOUD_PROJECT')
+  if project and (project.startswith('s~') or project.startswith('e~')):
+    project = project[2:]
+  region = _get_region()
+
+  parent = client.queue_path(project, region, queue_name)
+
+  # Construct the target URL using HTTP request to bypass GAE routing bug
+  default_hostname = app_identity.get_default_version_hostname()
+  target_service = task.target or os.environ.get('GAE_SERVICE')
+
+  # Workaround for SDK bug that extracts service name with trailing '-dot'
+  if target_service and target_service.endswith('-dot'):
+    target_service = target_service[:-4]
+
+  if target_service and target_service != 'default':
+    url_host = f"{target_service}-dot-{default_hostname}"
+  else:
+    url_host = default_hostname
+
+  # Ensure task.url starts with /
+  relative_uri = task.url or '/'
+  if not relative_uri.startswith('/'):
+    relative_uri = '/' + relative_uri
+
+  url = f"https://{url_host}{relative_uri}"
+  print(f"Jetski: Constructed Cloud Tasks URL (refactored): {url}", flush=True)
+
+  http_method = tasks_v2beta3.HttpMethod.POST
+  if task.method:
+    method_map = {
+        'POST': tasks_v2beta3.HttpMethod.POST,
+        'GET': tasks_v2beta3.HttpMethod.GET,
+        'PUT': tasks_v2beta3.HttpMethod.PUT,
+        'DELETE': tasks_v2beta3.HttpMethod.DELETE,
+        'HEAD': tasks_v2beta3.HttpMethod.HEAD,
+    }
+    http_method = method_map.get(task.method, tasks_v2beta3.HttpMethod.POST)
+
+  headers = {}
+  if task.headers:
+    headers = dict(task.headers)
+
+  # Manually inject GAE headers for compatibility with the test app
+  headers['X-AppEngine-QueueName'] = queue_name
+  if task.name:
+    headers['X-AppEngine-TaskName'] = task.name
+
+  body = b''
+  if task.payload:
+    if isinstance(task.payload, str):
+      body = task.payload.encode('utf-8')
+    else:
+      body = task.payload
+
+  # Construct AppEngineHttpRequest
+  app_engine_http_request = {
+      'http_method': http_method,
+      'relative_uri': task.url or '/',
+      'body': body,
+      'headers': headers,
+  }
+
+  routing = {}
+  if target_service:
+    routing['service'] = target_service
+    # Also set version to see if it bypasses the regional routing bug
+    version = os.environ.get('GAE_VERSION')
+    if version:
+      routing['version'] = version
+    print(
+        f"Jetski: Using AppEngineHttpRequest with routing (refactored):"
+        f" service={target_service}, version={version}",
+        flush=True,
+    )
+  else:
+    print(
+        'Jetski: Using AppEngineHttpRequest with default routing'
+        ' (refactored)',
+        flush=True,
+    )
+
+  if routing:
+    app_engine_http_request['app_engine_routing'] = routing
+
+  ct_task = {'app_engine_http_request': app_engine_http_request}
+
+  if task.name:
+    ct_task['name'] = client.task_path(project, region, queue_name, task.name)
+
+  if task.eta:
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    eta = task.eta
+    if eta.tzinfo is not None:
+      eta = eta.astimezone(datetime.timezone.utc).replace(tzinfo=None)
+    delta = eta - epoch
+    seconds = int(delta.total_seconds())
+    nanos = int(delta.microseconds * 1000)
+    timestamp = Timestamp(seconds=seconds, nanos=nanos)
+    ct_task['schedule_time'] = timestamp
+
+  try:
+    response = client.create_task(request={'parent': parent, 'task': ct_task})
+    task_id = response.name.split('/')[-1]
+    task._Task__name = task_id
+    task._Task__queue_name = queue_name
+    task._Task__enqueued = True
+    if multiple:
+      return [task]
+    else:
+      return task
+  except google_exceptions.AlreadyExists as e:
+    from google.appengine.api.taskqueue.taskqueue import TaskAlreadyExistsError
+    raise TaskAlreadyExistsError(str(e))
+  except Exception as e:
+    raise e
--- src/google/appengine/api/taskqueue/taskqueue.py
+++ src/google/appengine/api/taskqueue/taskqueue.py
@@ -2141,8 +2141,9 @@ class Queue(object):
       if transactional:
         raise NotImplementedError(
             'Transactional tasks are not supported with CLOUD_TASK backend.')
-      result = self.__CallCloudTasksCreateTask(tasks[0], multiple)
-      return _DummyRPC(lambda: result)
+      from google.appengine.api.taskqueue import cloudtask
+      result = cloudtask.create_task_in_cloud_tasks(self.__name, tasks[0], multiple)
+      return cloudtask._DummyRPC(lambda: result)
 
     if has_push_task:
       fill_function = self.__FillAddPushTasksRequest
@@ -2221,126 +2222,6 @@ class Queue(object):
       return self.add_async(task, transactional).get_result()
     else:
       return []
-
-  def __CallCloudTasksCreateTask(self, task, multiple):
-    """Calls Cloud Tasks API to create a task."""
-    import os
-    import datetime
-    from google.cloud import tasks_v2beta3
-    from google.protobuf.timestamp_pb2 import Timestamp
-    from google.api_core import exceptions as google_exceptions
-    from google.appengine.api import app_identity
-
-    client = tasks_v2beta3.CloudTasksClient()
-    project = os.environ.get('GOOGLE_CLOUD_PROJECT')
-    if project and (project.startswith('s~') or project.startswith('e~')):
-      project = project[2:]
-    region = _get_region()
-
-    parent = client.queue_path(project, region, self.__name)
-
-    # Construct the target URL using HTTP request to bypass GAE routing bug
-    default_hostname = app_identity.get_default_version_hostname()
-    target_service = task.target or os.environ.get('GAE_SERVICE')
-    
-    # Workaround for SDK bug that extracts service name with trailing '-dot'
-    if target_service and target_service.endswith('-dot'):
-      target_service = target_service[:-4]
-      
-    if target_service and target_service != 'default':
-      url_host = f"{target_service}-dot-{default_hostname}"
-    else:
-      url_host = default_hostname
-    
-    # Ensure task.url starts with /
-    relative_uri = task.url or '/'
-    if not relative_uri.startswith('/'):
-      relative_uri = '/' + relative_uri
-      
-    url = f"https://{url_host}{relative_uri}"
-    print(f"Jetski: Constructed Cloud Tasks URL: {url}", flush=True)
-
-    http_method = tasks_v2beta3.HttpMethod.POST
-    if task.method:
-      method_map = {
-          'POST': tasks_v2beta3.HttpMethod.POST,
-          'GET': tasks_v2beta3.HttpMethod.GET,
-          'PUT': tasks_v2beta3.HttpMethod.PUT,
-          'DELETE': tasks_v2beta3.HttpMethod.DELETE,
-          'HEAD': tasks_v2beta3.HttpMethod.HEAD,
-      }
-      http_method = method_map.get(task.method, tasks_v2beta3.HttpMethod.POST)
-
-    headers = {}
-    if task.headers:
-      headers = dict(task.headers)
-
-    # Manually inject GAE headers for compatibility with the test app
-    headers['X-AppEngine-QueueName'] = self.__name
-    if task.name:
-      headers['X-AppEngine-TaskName'] = task.name
-
-    body = b''
-    if task.payload:
-      if isinstance(task.payload, str):
-        body = task.payload.encode('utf-8')
-      else:
-        body = task.payload
-
-    # Construct AppEngineHttpRequest
-    app_engine_http_request = {
-        'http_method': http_method,
-        'relative_uri': task.url or '/',
-        'body': body,
-        'headers': headers,
-    }
-
-    routing = {}
-    if target_service:
-      routing['service'] = target_service
-      # Also set version to see if it bypasses the regional routing bug
-      version = os.environ.get('GAE_VERSION')
-      if version:
-        routing['version'] = version
-      print(f"Jetski: Using AppEngineHttpRequest with routing: service={target_service}, version={version}", flush=True)
-    else:
-      print(f"Jetski: Using AppEngineHttpRequest with default routing", flush=True)
-
-    if routing:
-      app_engine_http_request['app_engine_routing'] = routing
-
-    ct_task = {
-        'app_engine_http_request': app_engine_http_request
-    }
-
-    if task.name:
-      ct_task['name'] = client.task_path(project, region, self.__name, task.name)
-
-    if task.eta:
-      epoch = datetime.datetime.utcfromtimestamp(0)
-      eta = task.eta
-      if eta.tzinfo is not None:
-        eta = eta.astimezone(datetime.timezone.utc).replace(tzinfo=None)
-      delta = eta - epoch
-      seconds = int(delta.total_seconds())
-      nanos = int(delta.microseconds * 1000)
-      timestamp = Timestamp(seconds=seconds, nanos=nanos)
-      ct_task['schedule_time'] = timestamp
-
-    try:
-      response = client.create_task(request={'parent': parent, 'task': ct_task})
-      task_id = response.name.split('/')[-1]
-      task._Task__name = task_id
-      task._Task__queue_name = self.__name
-      task._Task__enqueued = True
-      if multiple:
-        return [task]
-      else:
-        return task
-    except google_exceptions.AlreadyExists as e:
-      raise TaskAlreadyExistsError(str(e))
-    except Exception as e:
-      raise e
 
   def __AddTasks(self, tasks, transactional, fill_request, multiple, rpc=None):
     """Internal implementation of adding tasks where tasks must be a list."""
@@ -2729,44 +2610,3 @@ def add(*args, **kwargs):
       queue_name=queue_name, transactional=transactional)
 
 
-class _DummyRPC(object):
-  """A dummy RPC object to wrap synchronous calls for async compatibility."""
-
-  def __init__(self, result_provider):
-    self._result_provider = result_provider
-
-  def get_result(self):
-    return self._result_provider()
-
-  def wait(self):
-    pass
-
-  def check_success(self):
-    pass
-
-
-def _get_region():
-  """Determines the App Engine region."""
-  import os
-  import urllib.request
-  
-  region = os.environ.get('GAE_REGION')
-  if region:
-    return region
-
-  try:
-    req = urllib.request.Request(
-        'http://metadata.google.internal/computeMetadata/v1/instance/zone',
-        headers={'Metadata-Flavor': 'Google'}
-    )
-    with urllib.request.urlopen(req, timeout=1) as response:
-      zone = response.read().decode('utf-8')
-      if '/' in zone:
-        zone = zone.split('/')[-1]
-      region = zone.rsplit('-', 1)[0]
-      return region
-  except Exception:
-    pass
-
-  # Fallback to us-central1 if we can't detect it
-  return 'us-central1'
